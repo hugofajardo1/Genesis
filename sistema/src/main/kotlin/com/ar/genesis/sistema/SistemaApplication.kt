5a8e0446b5322202f2b7f210946fc4fb807bce6d
Hugo Fajardo <hugofajardo1@gmail.com>
2020-11-10 16:21:23 -0300

    commit 1

---
+++ sistema/src/main/kotlin/com/ar/genesis/sistema/SistemaApplication.kt
@@ -0,0 +1,11 @@
+package com.ar.genesis.sistema
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SistemaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SistemaApplication>(*args)
+}
