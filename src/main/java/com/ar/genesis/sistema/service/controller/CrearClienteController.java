900fa157efa5d82dc04b35a2819e2e28e490cb59
Hugo Fajardo <hugofajardo1@gmail.com>
2020-11-14 09:04:13 -0300

    Cambio minimo

--- src/main/java/com/ar/genesis/sistema/service/controller/CrearClienteController.java
+++ src/main/java/com/ar/genesis/sistema/service/controller/CrearClienteController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/")
 @RestController
 public class CrearClienteController {
--- src/main/java/com/ar/genesis/sistema/service/dto/ClienteDTO.java
+++ src/main/java/com/ar/genesis/sistema/service/dto/ClienteDTO.java
@@ -1,19 +1,24 @@
 package com.ar.genesis.sistema.service.dto;
 
 import com.ar.genesis.sistema.core.domain.Cliente;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ClienteDTO {
 
+    @JsonProperty("id")
     Integer id;
+    @JsonProperty("nombre")
     String nombre;
+    @JsonProperty("domicilio")
     String domicilio;
+    @JsonProperty("telefono")
     String telefono;
 
-    private ClienteDTO(Cliente unCliente) {
-        this.setId(unCliente.getId());
-        this.setNombre(unCliente.getNombre());
-        this.setDomicilio(unCliente.getDomicilio());
-        this.setTelefono(unCliente.getTelefono());
+    public ClienteDTO(Integer id, String nombre, String domicilio, String telefono) {
+        this.setId(id);
+        this.setNombre(nombre);
+        this.setDomicilio(domicilio);
+        this.setTelefono(telefono);
     }
 
     public Integer getId() {
