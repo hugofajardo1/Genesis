dfcab81d3dd692b13af3408c07c9a4f1fa8fa01c
Hugo Fajardo <hugofajardo1@gmail.com>
2020-11-22 08:27:09 -0300

    ModificarRubroControllerUT

---
+++ src/main/java/com/ar/genesis/sistema/service/controller/ModificarRubroController.java
@@ -0,0 +1,35 @@
+package com.ar.genesis.sistema.service.controller;
+
+import com.ar.genesis.sistema.core.domain.Rubro;
+import com.ar.genesis.sistema.core.exception.RubroExisteException;
+import com.ar.genesis.sistema.core.input.IModificarRubroInput;
+import com.ar.genesis.sistema.service.dto.RubroDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class ModificarRubroController {
+    @Inject
+    IModificarRubroInput iModificarRubroInput;
+
+    public ModificarRubroController(IModificarRubroInput iModificarRubroInput) {
+        this.iModificarRubroInput = iModificarRubroInput;
+    }
+    @PutMapping(value = "/Rubro")
+    public ResponseEntity<?> modificarRubro(@RequestBody RubroDTO RubroDTO){
+        try{
+            boolean resultado = this.iModificarRubroInput.modificarRubro(Rubro.instancia(RubroDTO.getId(), RubroDTO.getNombre()));
+            if (resultado) return ResponseEntity.status(HttpStatus.OK).body(true);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (RubroExisteException e) {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body(e.toString());
+        }
+    }
+}
