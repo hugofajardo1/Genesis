d6adbaef4a3a425fed2bac7f78051e8fdfc75550
Hugo Fajardo <hugofajardo1@gmail.com>
2020-11-29 08:06:51 -0300

    ObtenerLocalidadesControllerTest

---
+++ src/test/java/com/ar/genesis/sistema/controllerUnitTests/ObtenerLocalidadesControllerTest.java
@@ -0,0 +1,37 @@
+package com.ar.genesis.sistema.controllerUnitTests;
+
+import com.ar.genesis.sistema.core.domain.Localidad;
+import com.ar.genesis.sistema.core.input.IObtenerLocalidadesInput;
+import com.ar.genesis.sistema.service.controller.ObtenerLocalidadesController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class ObtenerLocalidadesControllerTest {
+    @Mock
+    IObtenerLocalidadesInput obtenerLocalidadesInput;
+
+    @Test
+    public void obtenerLocalidades_LocalidadesExisten_Devuelve200(){
+        List<Localidad> localidades = new ArrayList<>();
+        localidades.add(Localidad.instancia(1,"Localidad 1"));
+        when(obtenerLocalidadesInput.obtenerLocalidades()).thenReturn(localidades);
+
+        ObtenerLocalidadesController obtenerLocalidadesController = new ObtenerLocalidadesController(obtenerLocalidadesInput);
+        ResponseEntity<?> responseEntity = obtenerLocalidadesController.obtenerLocalidades();
+        List<?> resultado = (List<?>) responseEntity.getBody();
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        if(resultado != null) {
+            Assertions.assertEquals(resultado.size(), 1);
+        }
+    }
+}
