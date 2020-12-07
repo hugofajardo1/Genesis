4f21bc6028fd3e89713fa76fe3d31781fdd427c0
Hugo Fajardo <hugofajardo1@gmail.com>
2020-12-07 18:59:35 -0300

    UbicacionObtenerPorIdControllerTest

---
+++ src/test/java/com/ar/genesis/sistema/controllerUnitTests/UbicacionObtenerPorIdControllerTest.java
@@ -0,0 +1,40 @@
+package com.ar.genesis.sistema.controllerUnitTests;
+
+import com.ar.genesis.sistema.core.domain.Ubicacion;
+import com.ar.genesis.sistema.core.exception.UbicacionNoExisteException;
+import com.ar.genesis.sistema.core.input.IUbicacionObtenerPorIdInput;
+import com.ar.genesis.sistema.service.controller.UbicacionObtenerPorIdController;
+import com.ar.genesis.sistema.service.dto.UbicacionDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class UbicacionObtenerPorIdControllerTest {
+    @Mock
+    IUbicacionObtenerPorIdInput miUbicacionObtenerPorIdInput;
+
+    @Test
+    public void obtenerUbicacionPorId_DevuelveUbicacion() throws UbicacionNoExisteException {
+        Ubicacion unUbicacion = Ubicacion.instancia(1,"Ubicacion 1");
+        when(miUbicacionObtenerPorIdInput.obtenerUbicacion(1)).thenReturn(unUbicacion);
+
+        UbicacionObtenerPorIdController ubicacionObtenerPorIdController = new UbicacionObtenerPorIdController(miUbicacionObtenerPorIdInput);
+        ResponseEntity<?> responseEntity = ubicacionObtenerPorIdController.obtenerUbicacion(1);
+        UbicacionDTO resultado = (UbicacionDTO) responseEntity.getBody();
+        Assertions.assertNotNull(resultado);
+    }
+    @Test
+    public void obtenerUbicacionPorId_DevuelveNull() throws UbicacionNoExisteException {
+        when(miUbicacionObtenerPorIdInput.obtenerUbicacion(1)).thenReturn(null);
+
+        UbicacionObtenerPorIdController ubicacionObtenerPorIdController = new UbicacionObtenerPorIdController(miUbicacionObtenerPorIdInput);
+        ResponseEntity<?> responseEntity = ubicacionObtenerPorIdController.obtenerUbicacion(1);
+        UbicacionDTO resultado = (UbicacionDTO) responseEntity.getBody();
+        Assertions.assertNull(resultado);
+    }
+}
