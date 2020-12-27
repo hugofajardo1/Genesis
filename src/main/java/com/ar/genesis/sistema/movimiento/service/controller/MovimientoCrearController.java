8ab947ebd9b0e591d800cde08bc9a65f6e934fc6
Hugo Fajardo <hugofajardo1@gmail.com>
2020-12-27 18:11:05 -0300

    Movimiento ControllerTests

--- src/main/java/com/ar/genesis/sistema/movimiento/service/controller/MovimientoCrearController.java
+++ src/main/java/com/ar/genesis/sistema/movimiento/service/controller/MovimientoCrearController.java
@@ -3,15 +3,9 @@ package com.ar.genesis.sistema.movimiento.service.controller;
 import com.ar.genesis.sistema.movimiento.core.exception.MovimientoExisteException;
 import com.ar.genesis.sistema.movimiento.core.exception.MovimientoIncompletoException;
 import com.ar.genesis.sistema.movimiento.core.input.IMovimientoCrearInput;
-import com.ar.genesis.sistema.ficha.core.domain.Ficha;
-import com.ar.genesis.sistema.localidad.core.domain.Localidad;
 import com.ar.genesis.sistema.movimiento.core.domain.Movimiento;
-import com.ar.genesis.sistema.provincia.core.domain.Provincia;
 import com.ar.genesis.sistema.movimiento.service.dto.MovimientoDTO;
-import com.ar.genesis.sistema.puntoventa.core.domain.PuntoVenta;
-import com.ar.genesis.sistema.sucursal.core.domain.Sucursal;
-import com.ar.genesis.sistema.tipoiva.core.domain.TipoIva;
-import com.ar.genesis.sistema.tipomovimiento.core.domain.TipoMovimiento;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -34,14 +28,8 @@ public class MovimientoCrearController {
     @PostMapping(value = "/movimiento")
     public ResponseEntity<?> crearMovimiento(@RequestBody MovimientoDTO unMovimientoDTO){
         try{
-            PuntoVenta unPuntoVenta = PuntoVenta.instancia(unMovimientoDTO.getPuntoVenta().getId(), unMovimientoDTO.getPuntoVenta().getNombre(), Sucursal.instancia(unMovimientoDTO.getSucursal().getId(), unMovimientoDTO.getSucursal().getNombre()), 5);
-            TipoIva unTipoIva = TipoIva.instancia(unMovimientoDTO.getFicha().getTipoIva().getId(), unMovimientoDTO.getFicha().getTipoIva().getNombre());
-            TipoMovimiento unTipoMovimiento = TipoMovimiento.instancia(unMovimientoDTO.getTipoMovimiento().getId(), unMovimientoDTO.getTipoMovimiento().getNombre(), "Debe");
-            Localidad unaLocalidad = Localidad.instancia(unMovimientoDTO.getFicha().getLocalidad().getId(), unMovimientoDTO.getFicha().getLocalidad().getNombre());
-            Provincia unaProvincia = Provincia.instancia(unMovimientoDTO.getFicha().getProvincia().getId(), unMovimientoDTO.getFicha().getProvincia().getNombre());
-            Ficha unaFicha = Ficha.instancia(unMovimientoDTO.getFicha().getId(), unMovimientoDTO.getFicha().getNombre(), unMovimientoDTO.getFicha().getDomicilio(), unaLocalidad, unaProvincia, unMovimientoDTO.getFicha().getTelefono(), unTipoIva, unMovimientoDTO.getFicha().getCuit(), unMovimientoDTO.getFicha().getIbrutos(), unMovimientoDTO.getFicha().getContacto());
-
-            boolean resultado = this.miMovimientoCrearInput.crearMovimiento(Movimiento.instancia(unMovimientoDTO.getId(), unPuntoVenta, unTipoMovimiento, unaFicha, unTipoIva, unaFicha.getCuit(), unMovimientoDTO.getFecha(), unMovimientoDTO.getHora(), unMovimientoDTO.getMontoNeto(), unMovimientoDTO.getMontoTotal(), unMovimientoDTO.getSaldo()));
+            ModelMapper modelMapper = new ModelMapper();
+            boolean resultado = this.miMovimientoCrearInput.crearMovimiento(modelMapper.map(unMovimientoDTO, Movimiento.class));
             if (resultado) return ResponseEntity.status(HttpStatus.OK).body(true);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         } catch (MovimientoExisteException | MovimientoIncompletoException e) {
--- src/main/java/com/ar/genesis/sistema/movimiento/service/controller/MovimientoObtenerPorIdController.java
+++ src/main/java/com/ar/genesis/sistema/movimiento/service/controller/MovimientoObtenerPorIdController.java
@@ -1,17 +1,9 @@
 package com.ar.genesis.sistema.movimiento.service.controller;
 
-import com.ar.genesis.sistema.ficha.service.dto.FichaDTO;
-import com.ar.genesis.sistema.localidad.service.dto.LocalidadDTO;
 import com.ar.genesis.sistema.movimiento.core.domain.Movimiento;
 import com.ar.genesis.sistema.movimiento.core.exception.MovimientoNoExisteException;
 import com.ar.genesis.sistema.movimiento.core.input.IMovimientoObtenerPorIdInput;
 import com.ar.genesis.sistema.movimiento.service.dto.MovimientoDTO;
-import com.ar.genesis.sistema.movimiento.service.dto.MovimientoItemDTO;
-import com.ar.genesis.sistema.producto.service.dto.ProductoDTO;
-import com.ar.genesis.sistema.provincia.service.dto.ProvinciaDTO;
-import com.ar.genesis.sistema.puntoventa.service.dto.PuntoVentaDTO;
-import com.ar.genesis.sistema.tipoiva.service.dto.TipoIvaDTO;
-import com.ar.genesis.sistema.tipomovimiento.service.dto.TipoMovimientoDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -38,16 +28,6 @@ public class MovimientoObtenerPorIdController {
             Movimiento unMovimiento = miMovimientoObtenerPorIdInput.obtenerMovimiento(id);
             MovimientoDTO unMovimientoDTO=null;
             if (unMovimiento!=null){
-                /*PuntoVentaDTO unPuntoVenta = new PuntoVentaDTO(unMovimiento.getPuntoVenta().getId(), unMovimiento.getPuntoVenta().getNombre(), unMovimiento.getPuntoVenta().getSucursal(), unMovimiento.getPuntoVenta().getTipoFacturacion());
-                TipoMovimientoDTO unTipoMovimiento = new TipoMovimientoDTO(unMovimiento.getTipoMovimiento().getId(), unMovimiento.getTipoMovimiento().getNombre(), unMovimiento.getTipoMovimiento().getImputacion());
-                LocalidadDTO unaLocalidad = new LocalidadDTO(unMovimiento.getFicha().getLocalidad().getId(), unMovimiento.getFicha().getLocalidad().getNombre());
-                ProvinciaDTO unaProvincia = new ProvinciaDTO(unMovimiento.getFicha().getProvincia().getId(), unMovimiento.getFicha().getProvincia().getNombre());
-                TipoIvaDTO unTipoIva = new TipoIvaDTO(unMovimiento.getTipoIva().getId(), unMovimiento.getTipoIva().getNombre());
-                FichaDTO unaFicha = new FichaDTO(unMovimiento.getFicha().getId(), unMovimiento.getFicha().getNombre(), unMovimiento.getFicha().getDomicilio(), unaLocalidad, unaProvincia, unMovimiento.getFicha().getTelefono(), unTipoIva, unMovimiento.getFicha().getCuit(), unMovimiento.getFicha().getIbrutos(), unMovimiento.getFicha().getContacto());
-                List<MovimientoItemDTO> items = new ArrayList<>();
-                unMovimiento.getItems().forEach(it -> items.add(new MovimientoItemDTO(it.getId(), new ProductoDTO())));
-                unMovimientoDTO = new MovimientoDTO(unMovimiento.getId(), unPuntoVenta, unTipoMovimiento, unaFicha, unTipoIva, unMovimiento.getCuit(), unMovimiento.getFecha(), unMovimiento.getHora(), unMovimiento.getMontoNeto(), unMovimiento.getMontoTotal(), unMovimiento.getSaldo(), unMovimiento.getItems());
-                 */
                 ModelMapper modelMapper = new ModelMapper();
                 unMovimientoDTO = modelMapper.map(unMovimiento, MovimientoDTO.class);
             }
